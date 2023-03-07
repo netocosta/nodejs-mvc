134f04f1974e6c065600979c6b46606c575d25dd
Neto Costa <netocjp@gmail.com>
2023-03-07 12:47:39 -0300

    Edicao e Remocao de cliente

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -3,13 +3,22 @@ const { crypto } = require('../utils/password')
 
 const defaultTitle = 'Cadastro de Clientes'
 
-function index(req, res) {
+async function list(req, res) {
+  const users = await CustomersModel.find()
+
+  res.render('list', {
+    title: 'Listagem de Usuarios',
+    users,
+  })
+}
+
+function addForm(req, res) {
   res.render('register', {
     title: defaultTitle
   })
 }
 
-async function add(req, res) {
+async function addAction(req, res) {
   const { name, age, email, password } = req.body
 
   const passwordCrypto = await crypto(password)
@@ -29,19 +38,51 @@ async function add(req, res) {
   })
 }
 
+async function editForm(req, res) {
+  const { id } = req.query
 
-async function listUsers(req, res) {
-  const users = await CustomersModel.find()
+  const user = await CustomersModel.findById(id)
 
-  res.render('list', {
-    title: 'Listagem de Usuarios',
-    users
+  res.render('edit', {
+    title: 'Editar Usuario',
+    user,
   })
 }
 
+async function editAction(req, res) {
+  const { name, age, email } = req.body
+  const { id } = req.params
+
+  const user = await CustomersModel.findById(id)
+
+  user.name = name
+  user.age = age
+  user.email = email
+
+  user.save()
+
+  res.render('edit', {
+    title: defaultTitle,
+    user,
+    message: 'Usuario atualizado com sucesso'
+  })
+}
+
+async function removeAction(req, res) {
+  const { id } = req.params
+
+  const remove = await CustomersModel.deleteOne({ _id: id })
+
+  if (remove.deletedCount) {
+    res.redirect('/list')
+  }
+}
 
 module.exports = {
-  index,
-  add,
-  listUsers
+  list,
+  addForm,
+  addAction,
+  editForm,
+  editAction,
+  removeAction,
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,10 +7,11 @@ const IndexController = require('../controllers/index')
 router.get('/', IndexController.index)
 
 // registro
-router.get('/register', CustomersController.index)
-router.post('/register/add', CustomersController.add)
-
-// listar
-router.get('/list', CustomersController.listUsers)
+router.get('/list', CustomersController.list)
+router.get('/register', CustomersController.addForm)
+router.post('/register/add', CustomersController.addAction)
+router.get('/register/edit', CustomersController.editForm)
+router.post('/register/edit/:id', CustomersController.editAction)
+router.get('/register/remove/:id', CustomersController.removeAction)
 
 module.exports = router
